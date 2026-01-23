d89d994860096e51e6b79ce12cf5e1215e5f8ce0
Israt-Nisha <isratnisha1999@gmail.com>
2026-01-23 23:23:03 +0600

    crate service layer

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { userService } from "@/services/user.service";
 
 export default async function Home() {
 
-  const session = await authClient.getSession();
+   const { data } = await userService.getSession();
 
-  console.log(session);
+  console.log(data);
 
   return (
     <div>
