41061f6ce8ee19e86a27517b4d31a47dcc0e7ddf
Israt-Nisha <isratnisha1999@gmail.com>
2026-01-24 21:46:21 +0600

    .env set

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,8 @@
 
+import { env } from "@/env";
 import { cookies } from "next/headers";
 
-
+const AUTH_URL = env.API_URL;
 
 export const userService = {
     getSession: async function () {
@@ -10,7 +11,7 @@ export const userService = {
 
             console.log(cookieStore.toString());
 
-            const res = await fetch("http://localhost:5000/api/auth/get-session", {
+            const res = await fetch(`${AUTH_URL}/get-session`, {
                 headers: {
                     Cookie: cookieStore.toString(),
                 },
