c5d75f5c31c45842955ad8ace0dc946131ec32c6
Israt-Nisha <isratnisha1999@gmail.com>
2026-01-26 10:49:28 +0600

    implementing tanstack from

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
+import { getBlogs } from "@/actions/blog.action";
+import { useEffect, useState } from "react";
+
 export default function AboutPage() {
+  const [data, setData] = useState();
+  const [error, setError] = useState<{ message: string } | null>(null);
 
-    // await new Promise((resolve) => setTimeout(resolve, 4000));
+  console.log(data);
+  console.log(error);
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
 
-    // throw new Error("SomeThing went wrong")
+      setData(data);
+      setError(error);
+    })();
+  }, []);
+
 
     return (
         <div>
