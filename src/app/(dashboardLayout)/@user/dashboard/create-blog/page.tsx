aac396549a63e4958d8b945bf910806341a4ffd1
Israt-Nisha <isratnisha1999@gmail.com>
2026-01-26 23:09:42 +0600

    create Blog server from

---
+++ src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
@@ -0,0 +1,12 @@
+import CreateBlogFormServer from '@/components/modules/user/createBlog/CreateBlogFormServer';
+import React from 'react';
+
+const CreateBlog = () => {
+    return (
+        <div>
+            <CreateBlogFormServer/>
+        </div>
+    );
+};
+
+export default CreateBlog;
